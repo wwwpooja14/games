2b1e1156aaa7b76f1948dd3f407685c350af99c1
Pooja Singh <poojasingh99ftd@gmail.com>
2020-10-22 11:24:45 +0530

    updatefiles

--- tictac.js
+++ tictac.js
@@ -1,15 +1,18 @@
-var turn = 1;
+document.addEventListener("DOMContentLoder", () => {
+    document.querySelector("gameboard").addEventListener("click", handleClick);
+    let currentState = "o";
 
+    function handleClick(e) {
+        let targetCell = e.target;
 
-function playerAction(element) {
-    console.log(element);
-    if (turn == 1) {
-        element.innerHtml = "X";
-        document.getElementById('messagesection').innerHtml = "Player 2 turn";
-        turn = 2;
-    } else if (turn == 2) {
-        element.innerHtml = "O";
-        document.getElementById('messagesection').innerHtml = "Player 1 turn";
-        turn = 1;
+        if (currentState === "O") {
+            currentState = "X";
+
+        } else {
+            currentState = "O";
+        }
+        if (targetCell.innerHTML == "") {
+            targetCell.innerHTML = currentState;
+        }
     }
-}
+})
